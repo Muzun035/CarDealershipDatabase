7b76a255a4518a9a1028b2df977b1eff9bae368e
Muzun <muzun@pgh.yearup.org>
2024-11-26 13:47:32 -0500

    sales_contracts table created

--- CarDealershipDatabase.sql
+++ CarDealershipDatabase.sql
@@ -30,3 +30,18 @@ CREATE TABLE inventory (
     FOREIGN KEY (dealership_id) REFERENCES dealerships(dealership_id) ON DELETE CASCADE,
     FOREIGN KEY (vin) REFERENCES vehicles(vin) ON DELETE CASCADE
 );
+
+CREATE TABLE sales_contracts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    VIN VARCHAR(17),
+    customer_name VARCHAR(50),
+    sale_date DATE,
+    sale_price DECIMAL(10, 2),
+    SALES_TAX_RATE DECIMAL(4, 2),
+    RECORDING_FEE DECIMAL(10, 2),
+    processingFee DECIMAL(10, 2),
+    financeOption BOOLEAN,
+    salesTaxAmount DECIMAL(10, 2),
+    FOREIGN KEY (VIN) REFERENCES vehicles(VIN)
+);
+
